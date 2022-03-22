bbbe1f462abf5a832297047cd05a532987d7c48a
Syafiq-ma <alaudinsyafiq910@gmail.com>
2022-03-22 14:16:04 +0700

    add get data feature

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
-const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 5000);
+
+var request = require("request")
+const moment = require('moment')
+
+app.get('/api/data', (req,res) => {
+    request({
+        url:'https://api.flickr.com/services/feeds/photos_public.gne?format=json',
+        json: true
+    }, function(error, response, body){
+        var mySubString = body.substring(
+            body.indexOf("[") - 1,
+            body.lastIndexOf("]") + 1
+        )
+        var data = JSON.parse(mySubString)
+        allPhotos = data.map(photo => ({
+
+            photoStaticURL: photo.media.m.toString().replace("\\", ""),
+            PhotoFlickrURL: photo.link.toString().replace("\\", ""),
+            photoOwnerURL: 'https://www.flickr.com/photos/' + photo.author_id,
+            photoTitle: photo.title,
+            photoPublushed: moment(photo.published).format('MM MMMM YYYY'),
+            photoOwner: photo.author.substring(
+                photo.author.indexOf("\"") + 1,
+                photo.author.lastIndexOf("\"") - 1
+            ),
+            photoTags: photo.tags
+        }))
+        res.send(allPhotos) 
+    })
+})
